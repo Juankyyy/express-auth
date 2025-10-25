99ef9335c8240d4db1a6d8f7eece510728f39c36
Juankyyy <juancamiloherrera960@gmail.com>
2025-10-24 20:05:07 -0500

    feat(post): create and get by id model

--- models/post.model.js
+++ models/post.model.js
@@ -14,4 +14,50 @@ export class PostModel {
       console.error("Error al buscar posts:", err);
     }
   }
+
+  static async getById(req, res) {
+    try {
+      const { id } = req.params;
+      const post = await PostController.getById({ id });
+
+      return res.status(200).json(post);
+    } catch (err) {
+      console.error("Error al buscar post:", err);
+    }
+  }
+
+  static async create(req, res) {
+    try {
+      const { userId, description, images, likes, comments, visibility } =
+        req.body;
+
+      const validation = validatePost({
+        userId,
+        description,
+        images,
+        likes,
+        comments,
+        visibility,
+      });
+
+      if (!validation.success) {
+        return res
+          .status(400)
+          .json({ message: JSON.parse(validation.error.message) });
+      }
+
+      const post = await PostController.create({
+        userId,
+        description,
+        images,
+        likes,
+        comments,
+        visibility,
+      });
+
+      res.status(201).json({ message: "Post creado", post: post });
+    } catch (err) {
+      console.error("Error al crear post:", err);
+    }
+  }
 }
--- routes/posts.route.js
+++ routes/posts.route.js
@@ -4,3 +4,7 @@ import { PostModel } from "../models/post.model.js";
 export const PostsRouter = Router();
 
 PostsRouter.get("/", PostModel.getAll);
+
+PostsRouter.get("/:id", PostModel.getById);
+
+PostsRouter.post("/", PostModel.create);
